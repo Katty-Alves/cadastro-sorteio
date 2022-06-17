adc223d7d5bdd0b28e97e482e3973a5d34b76ddf
katarina <kas10@aluno.ifal.edu.br>
2022-06-17 14:56:04 -0300

    aoo

--- app.js
+++ app.js
@@ -3,41 +3,36 @@ function Cadastrar() {
   var Nome = document.getElementById('name'); 
   var Email = document.getElementById('email');
   var Senha = document.getElementById('senha');
-  var Cidade = document.getElementById('cidade');
-  var Estado = document.getElementById('estado');
+  var Data_nascimento = document.getElementById('data_nascimento');
 
-   if (Nome.value.match(/\w+\s+/) == null){
+   if (Nome.value.length < 6){
 		document.getElementById('name').classList.add('erro');
     alert('Informe seu nome completo.')
     	return false;
   }
 
-  else if (Email.value.match(/\S+@\S+\.\S+/) == null){
+  else if (Email.value.indexOf("@") == -1 || Email.value.indexOf(".") == -1){
 		document.getElementById('email').classList.add('erro');
     alert('Informe um email válido.')
     	return false;
   }
 
-  else if (Senha.value.match(/[0-9]/g) == null || (!senha.match(/[A-Z]/g) && !senha.match(/[a-z]/g)) || !senha.match(/[\W|_]/g)){
+  else if (Senha.value.length < 6){
 		document.getElementById('senha').classList.add('erro');
     alert('Informe uma senha válida.')
     	return false;
   }
 
-  if (Cidade.value.match(/\w+\s+/) == null){
-		document.getElementById('cidade').classList.add('erro');
-    alert('Informe uma CIDADE.')
+  else if (Data_nascimento.value == ''){
+		document.getElementById('data_nascimento').classList.add('erro');
+    alert('Informe sua data de nascimento.')
     	return false;
-        }
-
-  else if (Estado.selectedIndex == 0){
-		document.getElementById('estado').classList.add('erro'); 
-    alert('Informe o estado')
-    	return false;
-
   }
 
-  else{
+else{
   window.location.href = "file:///C:/Users/Eu%20e%20o%20outro/Documents/cadastro-sorteio/katt-sorteio/sorteio.html";
 }
+
+
+
 }
